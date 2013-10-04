5a484443835e374d074635ab2ae6944ba7afe16f
xkings <xkingsgames@gmail.com>
2013-10-04 23:31:44 +0200

    Draft of rendering system for sprites added.

---
+++ PokemonTD/src/com/xkings/pokemontd/component/SpriteComponent.java
@@ -0,0 +1,27 @@
+package com.xkings.pokemontd.component;
+
+import com.artemis.Component;
+import com.badlogic.gdx.utils.Array;
+
+import static com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+
+/**
+ * Created by Tomas on 10/4/13.
+ */
+public class SpriteComponent extends Component {
+
+    private final Array<AtlasRegion> sprites;
+    private int position;
+
+    public SpriteComponent(Array<AtlasRegion> sprites) {
+        this.sprites = sprites;
+    }
+
+    public AtlasRegion getSprite() {
+        return sprites.get(position);
+    }
+
+    public void next() {
+        position = position + 1 % sprites.size;
+    }
+}
---
+++ PokemonTD/src/com/xkings/pokemontd/system/RenderSpriteSystem.java
@@ -0,0 +1,57 @@
+package com.xkings.pokemontd.system;
+
+import com.artemis.Aspect;
+import com.artemis.ComponentMapper;
+import com.artemis.Entity;
+import com.artemis.annotations.Mapper;
+import com.artemis.systems.EntityProcessingSystem;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.xkings.core.component.PositionComponent;
+import com.xkings.core.component.RotationComponent;
+import com.xkings.core.component.SizeComponent;
+import com.xkings.pokemontd.component.SpriteComponent;
+
+/**
+ * Created by Tomas on 10/4/13.
+ */
+public class RenderSpriteSystem  extends EntityProcessingSystem {
+    private final Camera camera;
+    private final SpriteBatch spriteBatch = new SpriteBatch();
+
+    @Mapper
+    ComponentMapper<PositionComponent> positionMapper;
+    @Mapper
+    ComponentMapper<SizeComponent> sizeMapper;
+    @Mapper
+    ComponentMapper<RotationComponent> rotationMapper;
+    @Mapper
+    ComponentMapper<SpriteComponent> spriteMapper;
+
+    public RenderSpriteSystem(Camera camera) {
+        super(Aspect.getAspectForAll(PositionComponent.class, SizeComponent.class, SpriteComponent.class));
+        this.camera = camera;
+    }
+
+    @Override
+    protected void process(Entity e) {
+        PositionComponent positionComponent = positionMapper.get(e);
+        Vector3 size = sizeMapper.get(e).getPoint();
+        TextureAtlas.AtlasRegion sprite = spriteMapper.get(e).getSprite();
+        spriteBatch.setProjectionMatrix(camera.combined);
+        spriteBatch.begin();
+        float x = positionComponent.getPoint().x - size.x / 2f;
+        float y = positionComponent.getPoint().y - size.y / 2f;
+        if (rotationMapper.has(e)) {
+            Vector3 rotation = rotationMapper.get(e).getPoint();
+            spriteBatch.draw(sprite, x, y, size.x / 2f, size.y / 2f, size.x, size.y, 1f, 1f, rotation.x);
+        } else {
+            spriteBatch.draw(sprite, x, y, size.x, size.y);
+        }
+        spriteBatch.end();
+    }
+
+}
