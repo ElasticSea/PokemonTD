0dc374be3b529282b04a98559e816749c57c8505
xkings <xkingsgames@gmail.com>
2013-10-04 21:41:34 +0200

    Application skeleton

---
+++ PokemonTD-desktop/src/com/xkings/pokemontd/Main.java
@@ -0,0 +1,16 @@
+package com.xkings.pokemontd;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "PokemonTD";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new App(), cfg);
+	}
+}
---
+++ PokemonTD-ios/Main.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using com.badlogic.gdx.backends.ios;
+using com.xkings.pokemontd;
+
+namespace com.xkings.pokemontd
+{		
+	public class Application
+	{
+		[Register ("AppDelegate")]
+		public partial class AppDelegate : IOSApplication {
+			public AppDelegate(): base(new App(), getConfig()) {
+
+			}
+
+			internal static IOSApplicationConfiguration getConfig() {
+				IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+				config.orientationLandscape = true;
+				config.orientationPortrait = false;
+				config.useAccelerometer = true;
+				config.useMonotouchOpenTK = true;
+				config.useObjectAL = true;
+				return config;
+			}
+		}
+		
+		static void Main (string[] args)
+		{
+			UIApplication.Main (args, null, "AppDelegate");
+		}
+	}
+}
