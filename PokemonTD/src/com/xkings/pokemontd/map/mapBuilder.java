abde2b2285c69c30e89cb801b9ccd041f94346b6
SedaQ <pavelseda@email.cz>
2013-10-05 12:06:08 +0200

    Create class mapBuilder

---
+++ PokemonTD/src/com/xkings/pokemontd/map/mapBuilder.java
@@ -0,0 +1,12 @@
+package com.xkings.pokemontd.map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Seda
+ * Date: 5.10.13
+ * Time: 12:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class mapBuilder {
+
+}
