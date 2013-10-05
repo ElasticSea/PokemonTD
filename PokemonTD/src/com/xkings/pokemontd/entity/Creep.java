a0ce3e82d5de8756495ac4864400a8cf81a5dca8
xkings <xkingsgames@gmail.com>
2013-10-05 12:08:20 +0200

    Entities structure altered.

--- PokemonTD/src/com/xkings/pokemontd/entity/Creep.java
+++ PokemonTD/src/com/xkings/pokemontd/entity/Creep.java
@@ -4,28 +4,30 @@ import com.artemis.World;
 import com.xkings.core.component.PositionComponent;
 import com.xkings.core.component.RotationComponent;
 import com.xkings.core.component.SizeComponent;
+import com.xkings.core.component.SpeedComponent;
 import com.xkings.core.entity.ConcreteEntity;
+import com.xkings.pokemontd.component.HealthComponent;
 import com.xkings.pokemontd.component.PathComponent;
 import com.xkings.pokemontd.component.SpriteComponent;
-
-import static com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
 
 /**
  * Created by Tomas on 10/5/13.
  */
 public class Creep extends ConcreteEntity {
 
-    public Creep(World world, AtlasRegion texture, float x, float y, int size) {
+    public Creep(CreepType creepType, World world, float x, float y) {
         super(world);
         addComponent(new PositionComponent(x, y, 0));
-        addComponent(new RotationComponent(0,0, 0));
+        addComponent(new RotationComponent(0, 0, 0));
         addComponent(new PathComponent(null));
-        addComponent(new SpriteComponent(texture));
-        addComponent(new SizeComponent(size, size, 0));
+        addComponent(new SpriteComponent(creepType.getTexture()));
+        addComponent(new SizeComponent(creepType.getSize(), creepType.getSize(), 0));
+        addComponent(new SpeedComponent(creepType.getSpeed()));
+        addComponent(new HealthComponent(creepType.getHealth()));
     }
 
-    public static void registerCreep(World world, TowerType towerType, float x, float y, int size) {
-        Creep creep = new Creep(world, towerType.getTexture(), x, y, size);
+    public static void registerCreep(World world, CreepType creepType, float x, float y) {
+        Creep creep = new Creep(creepType, world, x, y);
         creep.register();
     }
 }
