6071b128ebfb9f11c6ec86ddf59b109d81fc730b
xkings <xkingsgames@gmail.com>
2013-10-07 20:53:52 +0200

    Reverting changes 2.

---
+++ PokemonTD/src/com/xkings/pokemontd/input/EnhancedGestureProcessor.java
@@ -0,0 +1,25 @@
+package com.xkings.pokemontd.input;
+
+import com.badlogic.gdx.math.Vector2;
+import com.xkings.core.graphics.camera.CameraHandler;
+import com.xkings.core.input.GestureProcessor;
+import com.xkings.pokemontd.entity.TowerType;
+import com.xkings.pokemontd.manager.TowerManager;
+
+/**
+ * Created by Tomas on 10/7/13.
+ */
+public class EnhancedGestureProcessor extends GestureProcessor {
+    private final TowerManager towerManager;
+
+    public EnhancedGestureProcessor(TowerManager towerManager, CameraHandler camera) {
+        super(camera);
+        this.towerManager = towerManager;
+    }
+
+    @Override
+    public boolean tap(float x, float y, int count, int button) {
+        Vector2 scaledCoordinates = camera.screenToWorld(x, y);
+        return towerManager.createTower(TowerType.DefaultTower, (int)scaledCoordinates.x, (int) scaledCoordinates.y);
+    }
+}
---
+++ PokemonTD/src/com/xkings/pokemontd/manager/TowerManager.java
@@ -0,0 +1,29 @@
+package com.xkings.pokemontd.manager;
+
+import com.artemis.World;
+import com.xkings.core.pathfinding.Blueprint;
+import com.xkings.pokemontd.entity.Tower;
+import com.xkings.pokemontd.entity.TowerType;
+
+/**
+ * Created by Tomas on 10/7/13.
+ */
+public class TowerManager {
+
+    private final Blueprint blueprint;
+    private final World world;
+
+    public TowerManager(World world, Blueprint blueprint) {
+        this.world = world;
+        this.blueprint = blueprint;
+    }
+
+    public boolean createTower(TowerType towerType, int x, int y) {
+        System.out.println(x+" "+y);
+        if (blueprint.isWalkable(x, y )) {
+            Tower.registerTower(world, towerType, x, y);
+            return true;
+        }
+        return false;
+    }
+}
