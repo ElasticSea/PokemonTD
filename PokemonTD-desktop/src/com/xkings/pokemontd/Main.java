5cbeea24fd530e3677fc4a7baed958b138bafc11
xkings <xkingsgames@gmail.com>
2013-10-07 02:42:33 +0200

    Support for textures added.

--- PokemonTD-desktop/src/com/xkings/pokemontd/Main.java
+++ PokemonTD-desktop/src/com/xkings/pokemontd/Main.java
@@ -3,13 +3,28 @@ package com.xkings.pokemontd;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
+import java.io.File;
+import static com.badlogic.gdx.tools.imagepacker.TexturePacker2.Settings;
+import static com.badlogic.gdx.tools.imagepacker.TexturePacker2.process;
+
 public class Main {
 	public static void main(String[] args) {
+        Settings texturePackerSettings = new Settings();
+        texturePackerSettings.paddingX = 0;
+        texturePackerSettings.paddingY = 0;
+        texturePackerSettings.edgePadding = false;
+
+        String source = new File(".").getAbsoluteFile().getParentFile().getParentFile().getParentFile().toString() + "/unprocessed/textures/";
+        String destination = new File(".").toString() + "/data/textures/";
+        process(source, destination, "packed");
+
+        System.out.println(new File(".").getAbsoluteFile().getParentFile().getParentFile().getParentFile());
+
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "PokemonTD";
-		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.useGL20 = true;
+		cfg.width = 640;
+		cfg.height = 640;
 		
 		new LwjglApplication(new App(), cfg);
 	}
