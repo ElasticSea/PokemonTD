64a70317f83039c1bdf63ea7b6ff0d09d2684b30
xkings <xkingsgames@gmail.com>
2013-10-19 21:04:41 +0200

    updating core

---
+++ PokemonTD/src/com/xkings/pokemontd/component/CreepAbilityComponent.java
@@ -0,0 +1,11 @@
+package com.xkings.pokemontd.component;
+
+import com.artemis.Component;
+
+/**
+ * Created by Tomas on 9/8/13.
+ */
+public class CreepAbilityComponent extends Component {
+
+    private final CreepAbilityType creepAbilityType;
+}
