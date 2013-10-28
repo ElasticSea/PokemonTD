3c4e0f5f930259a371f786fe172e906a05a9f07c
xkings <xkingsgames@gmail.com>
2013-10-28 22:23:03 +0100

    Textures render size is multiple of their original size.

--- PokemonTD/src/com/xkings/pokemontd/graphics/ui/Icon.java
+++ PokemonTD/src/com/xkings/pokemontd/graphics/ui/Icon.java
@@ -3,6 +3,7 @@ package com.xkings.pokemontd.graphics.ui;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 
 /**
  * Created by Tomas on 10/8/13.
@@ -11,17 +12,21 @@ abstract class Icon extends InteractiveBlock {
 
     private final SpriteBatch spriteBatch;
     private final TextureAtlas.AtlasRegion texture;
+    private final float size;
+    private final Vector2 offset;
 
-    Icon(Rectangle rectangle, SpriteBatch spriteBatch, TextureAtlas.AtlasRegion texture) {
+    Icon(Rectangle rectangle, SpriteBatch spriteBatch, TextureAtlas.AtlasRegion texture, boolean pixelScale) {
         super(rectangle);
         this.spriteBatch = spriteBatch;
         this.texture = texture;
+        this.size = pixelScale ? ((int) width / texture.getRegionWidth()) * texture.getRegionWidth() : width;
+        this.offset = new Vector2((width - size) / 2, (height - size) / 2);
     }
 
     @Override
     public void render() {
         spriteBatch.begin();
-        spriteBatch.draw(texture, x, y, width, height);
+        spriteBatch.draw(texture, x + offset.x, y + offset.y, size, size);
         spriteBatch.end();
     }
 }
