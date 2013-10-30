2c9314d8a4f1c70e7db57287a757d18176568506
xkings <xkingsgames@gmail.com>
2013-10-30 21:28:02 +0100

    Added comparator to path for sorting creeps by progression.

--- PokemonTD/src/com/xkings/pokemontd/system/resolve/PickEntitySystem.java
+++ PokemonTD/src/com/xkings/pokemontd/system/resolve/PickEntitySystem.java
@@ -1,11 +1,20 @@
 package com.xkings.pokemontd.system.resolve;
 
 import com.artemis.Aspect;
+import com.artemis.ComponentMapper;
 import com.artemis.Entity;
 import com.artemis.EntitySystem;
+import com.artemis.annotations.Mapper;
 import com.artemis.utils.ImmutableBag;
+import com.xkings.pokemontd.component.PathComponent;
+import com.xkings.pokemontd.map.Path;
+
+import java.util.*;
 
 public abstract class PickEntitySystem extends EntitySystem implements PickEntity {
+
+    @Mapper
+    ComponentMapper<PathComponent> pathMapper;
 
     private final boolean sort;
 
@@ -24,10 +33,22 @@ public abstract class PickEntitySystem extends EntitySystem implements PickEntit
     @Override
     protected final void processEntities(ImmutableBag<Entity> entities) {
         if (sort) {
-
-        }
-        for (int i = 0, s = entities.size(); s > i; i++) {
-            process(entities.get(i));
+            List<Path> paths = new ArrayList<Path>(entities.size());
+            Map<Path, Entity> map = new HashMap<Path, Entity>();
+            for (int i = 0; i < entities.size(); i++) {
+                Entity entity = entities.get(i);
+                PathComponent pathComponent = pathMapper.get(entity);
+                paths.add(pathComponent.getPath());
+                map.put(pathComponent.getPath(), entity);
+            }
+            Collections.sort(paths,Collections.reverseOrder());
+            for (Path path : paths) {
+                process(map.get(path));
+            }
+        } else {
+            for (int i = 0, s = entities.size(); s > i; i++) {
+                process(entities.get(i));
+            }
         }
     }
 
