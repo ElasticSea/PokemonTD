0e0a2d80b0c97c50b6dc994c8366bd2e3a237d0f
SedaQ <pavelseda@email.cz>
2013-11-11 00:58:13 +0100

    Effect abilities.

--- PokemonTD/src/com/xkings/pokemontd/component/attack/EffectName.java
+++ PokemonTD/src/com/xkings/pokemontd/component/attack/EffectName.java
@@ -4,5 +4,7 @@ package com.xkings.pokemontd.component.attack;
  * Created by Tomas on 11/10/13.
  */
 public enum EffectName {
-    Default
+    Wave, Burn, Entangle, Peck, Terrify, Haste, Sunbeam, Weaken, Freeze, Incinerate,
+    Stomp, Steal, Boulder, Impair, Charm, Shatter, Blaze, Corrode, LifeSteal, Infect,
+    SoundWave, Thunderbolt, Quake, Magma, Puzzle
 }
