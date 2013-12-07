917b7251bbb640a391b5e53c192641de6b2b701c
xkings <xkingsgames@gmail.com>
2013-12-07 18:44:27 +0100

    Difficulty changes.

--- PokemonTD/src/com/pixelthieves/pokemontd/Difficulty.java
+++ PokemonTD/src/com/pixelthieves/pokemontd/Difficulty.java
@@ -4,7 +4,7 @@ package com.pixelthieves.pokemontd;
  * Created by Tomas on 11/26/13.
  */
 public enum Difficulty {
-    Easy(1f, 1f), Normal(1.5f, 2f), Hard(2.5f, 7.5f), Insane(3f, 12f);
+    Easy(0.7f, 0.5f), Normal(0.7f, 1f), Hard(1f, 2f), Insane(1.75f, 7.5f), Impossible(2.1f, 12f);
     private final float multiplyer;
     private final float bonus;
 
@@ -18,6 +18,6 @@ public enum Difficulty {
     }
 
     public float getBonus() {
-        return multiplyer;
+        return bonus;
     }
 }
